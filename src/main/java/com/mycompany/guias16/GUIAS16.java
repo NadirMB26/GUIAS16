9caecd7fd415c3adcd4c990bb25c8b039dccb57f
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 19:07:55 -0500

    primer GUIA16

---
+++ src/main/java/com/mycompany/guias16/GUIAS16.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guias16;
+
+import igui.pantallas16;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUIAS16 {
+
+    public static void main(String[] args) {
+            new pantallas16().setVisible(true);
+    }
+}
